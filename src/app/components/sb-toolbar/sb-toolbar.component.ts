0294e48333d3faf1a7283d351c6d18137cfc3ad1
Arturo Alcalá <alcala28043@gmail.com>
2022-11-04 19:31:08 +0100

    toolbar

--- src/app/components/sb-toolbar/sb-toolbar.component.ts
+++ src/app/components/sb-toolbar/sb-toolbar.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'sb-toolbar',
-  templateUrl: './sb-toolbar.component.html',
-  styleUrls: ['./sb-toolbar.component.scss'],
+  selector: "sb-toolbar",
+  templateUrl: "./sb-toolbar.component.html",
+  styleUrls: ["./sb-toolbar.component.scss"],
 })
 export class SbToolbarComponent implements OnInit {
-  constructor() {}
+  public isMobileDisplay: boolean = true;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe(["(max-width: 768px)"])
+      .subscribe((result: BreakpointState) => {
+        if (result.matches) {
+          this.isMobileDisplay = true;
+        } else {
+          this.isMobileDisplay = false;
+        }
+      });
+  }
 
   ngOnInit(): void {}
 }
